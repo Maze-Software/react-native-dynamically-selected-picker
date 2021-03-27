eebf1044573d9bd00d40453bdd8bcaf27b58e317
Fırat Doğan <firat934@gmail.com>
2021-03-27 18:29:53 +0300

    Update PickerListItem.js

--- src/PickerListItem.js
+++ src/PickerListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function PickerListItem({
   label,
@@ -7,15 +7,19 @@ export default function PickerListItem({
   itemColor,
   allItemsColor,
   fontSize,
-  fontFamily = 'Arial',
+  selected,
+  fontFamily = 'SFUIDisplay-Bold',
 }) {
+  // console.log("sel", selected)
   return (
     <View style={style}>
       <Text
         style={{
-          fontSize: fontSize,
-          color: itemColor ? itemColor : allItemsColor,
-          fontFamily: fontFamily,
+          fontSize: selected ? fontSize : fontSize - 17,
+          fontWeight: selected ? 'bold' : '100',
+          color: selected ? 'green' : itemColor ? itemColor : allItemsColor,
+
+          fontFamily: selected ? 'SFUIDisplay-Bold' : 'SFUIDisplay-Regular',
         }}>
         {label}
       </Text>
